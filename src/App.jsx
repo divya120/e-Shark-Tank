3026c129a2ddd2ca19411ab5ea4d13c9eef03e79
AnuMessi10 <anuragdyadav2001@gmail.com>
2022-02-10 23:49:12 +0530

    added idea detail page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Button, Card, Grid, Rating, Typography } from '@mui/material';
 import sharkTankBG from './images/sharkTankBG.jpg';
 import start from './images/start.jpg';
 import { useState } from 'react';
+import IdeaDetail from './Components/IdeaDetail'
 
 
 // Width of the card component 
@@ -16,35 +17,36 @@ const App = () => {
   const [starValue, setStarValue] = useState(3);
   
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: "#15202B", position: "fixed", top: 0, bottom: 0, right: 0, left: 0, zIndex: -1}}>
       <ResponsiveAppBar />
-      <img src={sharkTankBG} alt="Landing Background" style={{ justifyContent: "center", position: "fixed", top: 0, bottom: 0, right: 0, left: 0, zIndex: -1, maxWidth:"100%" }} />
-      <Box> <Typography align="center" variant="h4" sx={{ fontFamily: "Roboto", color: "#483492", marginTop: 10 }}>Come pitch in your ideas, business models and demonstrate your marketing skills</Typography> </Box>
-      <Box>
+      {/* <img src={sharkTankBG} alt="Landing Background" style={{ justifyContent: "center", position: "fixed", top: 0, bottom: 0, right: 0, left: 0, zIndex: -1, maxWidth:"100%" }} /> */}
+      {/* <Box> 
+        <Typography align="center" variant="h4" sx={{ fontFamily: "Roboto", color: "#fff", marginTop: 10 }}>Come pitch in your ideas, business models and demonstrate your marketing skills</Typography> 
+      </Box> */}
+      <IdeaDetail />
+      {/* <Box>
         <Grid container spacing={5}>
-          <Grid item xs={3}>
-            <Card raised={true} sx={{ padding: 2 }}>
+          {[0, 1, 2].map(() => {return (<Grid item xs={3} display="flex" margin="0 auto">
+            <Card sx={{ padding: 2, backgroundColor: "#22303C", border: 'solid 1px #2F3336' }}>
               <Box display="flex" flexDirection="column">
-                <img src={start} alt="Startup Image" />
-                <Typography variant="h5">Idea Title</Typography>
-                <Typography>by Owner Name</Typography>
+                <img src={start} alt="startup" />
+                <Typography variant="h5" sx={{color: "#fff"}}>Idea Title</Typography>
+                <Typography sx={{color: "#fff"}}>by Owner Name</Typography>
               </Box>
               <Box>
-                <Typography variant="body1"> Event Description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio consequuntur deserunt ex voluptatum quasi tempora mollitia, optio doloremque illum veniam fugit in quaerat laborum, quos quibusdam, natus magnam consectetur atque.</Typography>
+                <Typography variant="body1" sx={{color: "#8899A6"}}> Event Description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio consequuntur deserunt ex voluptatum quasi tempora mollitia, optio doloremque illum veniam fugit in quaerat laborum, quos quibusdam, natus magnam consectetur atque.</Typography>
               </Box>
               <Button variant="contained">Invest</Button>
               <Rating name="simple-controlled"
                 value={starValue}
-                // onChange={(event, newValue) => {
-                //   setValue(newValue);
-                // }}
+                onChange={(event, newValue) => {
+                  setStarValue(newValue);
+                }}
               />
             </Card>
-          </Grid>
+          </Grid>)})}
         </Grid>
-
-      </Box>
-
+      </Box> */}
     </div>
   );
 }
