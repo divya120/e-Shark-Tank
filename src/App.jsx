c2f1d4f38a2c8b8c2921d1045903eb364723923e
AnuMessi10 <anuragdyadav2001@gmail.com>
2022-03-16 11:12:09 +0530

    chat UI implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
-import './App.css';
-import ResponsiveAppBar from './Components/AppBar';
-import Box from '@mui/material/Box';
-import { Button, Card, Grid, Rating, Typography } from '@mui/material';
-import sharkTankBG from './images/sharkTankBG.jpg';
-import start from './images/start.jpg';
-import { useState } from 'react';
-import IdeaDetail from './Components/IdeaDetail'
-import Landing from './Components/Landing';
-import CreatorProfile from './Components/CreatorProfile';
+import "./App.css";
+import ResponsiveAppBar from "./Components/AppBar";
+import Box from "@mui/material/Box";
+import { Button, Card, Grid, Rating, Typography } from "@mui/material";
+import sharkTankBG from "./images/sharkTankBG.jpg";
+import start from "./images/start.jpg";
+import { useState } from "react";
+import IdeaDetail from "./Components/IdeaDetail";
+import Landing from "./Components/Landing";
+import CreatorProfile from "./Components/CreatorProfile";
+import Chat from "./Components/Chat/Chat";
 
-
-// Width of the card component 
-// Caraousel 
-// Styling of components - inline 
+// Width of the card component
+// Caraousel
+// Styling of components - inline
 const App = () => {
-
-
   const [starValue, setStarValue] = useState(3);
-  
+
   return (
-    <div className="App" style={{
-      backgroundColor: "#15202B",
-      color: 'white',
-    //  position: "fixed",
-    top: 0, bottom: 0, right: 0, left: 0, zIndex: -1}}>
+    <div
+      className="App"
+      style={{
+        backgroundColor: "#15202B",
+        color: "white",
+        //  position: "fixed",
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        zIndex: -1,
+      }}
+    >
       <ResponsiveAppBar />
       {/* <img src={sharkTankBG} alt="Landing Background" style={{ justifyContent: "center", position: "fixed", top: 0, bottom: 0, right: 0, left: 0, zIndex: -1, maxWidth:"100%" }} /> */}
       {/* <Box> 
         <Typography align="center" variant="h4" sx={{ fontFamily: "Roboto", color: "#fff", marginTop: 10 }}>Come pitch in your ideas, business models and demonstrate your marketing skills</Typography> 
       </Box> */}
       {/* <IdeaDetail /> */}
-      <Landing />
+      {/* <Landing /> */}
+      <Chat />
       {/* <CreatorProfile /> */}
       {/* <Box>
         <Grid container spacing={5}>
@@ -57,6 +64,6 @@ const App = () => {
       </Box> */}
     </div>
   );
-}
+};
 
 export default App;
